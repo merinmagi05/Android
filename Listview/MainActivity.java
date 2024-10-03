25612a7dc8acc1aeae73c37011c77f54de5eb6d5
merinmagi05 <144668865+merinmagi05@users.noreply.github.com>
2024-10-03 12:41:26 +0530

    Update MainActivity.java

--- Listview/MainActivity.java
+++ Listview/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    TextView t1;
+    ListView l1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        t1 = findViewById(R.id.t1);
+        String msg = getIntent().getStringExtra("mykey");
+        if (msg == null) msg = "";
+        t1.setText("Welcome"+msg);
+        l1=findViewById(R.id.l1);
+        String[] courses = {"MCA", "MBA", "BBA", "BCA"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, courses);
+        l1.setAdapter(adapter);
+        l1.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        String itemname = l1.getItemAtPosition(i).toString();
+        Toast.makeText(this, itemname, Toast.LENGTH_SHORT).show();
     }
 }
