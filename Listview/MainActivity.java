a2af23ab73d80ae16f3b7e5e1de03deb3b28e9bb
merinmagi05 <144668865+merinmagi05@users.noreply.github.com>
2024-10-09 12:05:25 +0530

    Update MainActivity.java

--- Listview/MainActivity.java
+++ Listview/MainActivity.java
@@ -1,4 +1,5 @@
-package com.example.myapplication;
+package com.example.listview;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,32 +15,45 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    TextView t1;
-    ListView l1;
+    TextView t2;
+    ListView lv1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //taking text 2
+        t2=(TextView)findViewById(R.id.tv2);
+
+        //welcome message through an intent
+        String msg=getIntent().getStringExtra("mykey");
+        t2.setText("Welcome"+msg);
+        lv1=(ListView)findViewById(R.id.listview);
+        //Adding listview names to an array
+        String[] courses={"MCA","MBA","BCA","BBA"};
+        //storing to the List using ArrayAdapter
+        ArrayAdapter ad=new ArrayAdapter(this,android.R.layout.simple_list_item_1,courses);
+        lv1.setAdapter(ad);
+        lv1.setOnItemClickListener(this);
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        t1 = findViewById(R.id.t1);
-        String msg = getIntent().getStringExtra("mykey");
-        if (msg == null) msg = "";
-        t1.setText("Welcome"+msg);
-        l1=findViewById(R.id.l1);
-        String[] courses = {"MCA", "MBA", "BBA", "BCA"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, courses);
-        l1.setAdapter(adapter);
-        l1.setOnItemClickListener(this);
     }
+
+
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        String itemname = l1.getItemAtPosition(i).toString();
-        Toast.makeText(this, itemname, Toast.LENGTH_SHORT).show();
+            String itemname=lv1.getItemAtPosition(i).toString();
+            Toast.makeText(this,itemname, Toast.LENGTH_SHORT).show();
     }
 }
