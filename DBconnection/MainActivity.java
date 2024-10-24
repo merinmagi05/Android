ea6d3ccf997ff83b5f8fc7079649247fff38a2b1
merinmagi05 <144668865+merinmagi05@users.noreply.github.com>
2024-10-24 12:30:29 +0530

    Update MainActivity.java

--- DBconnection/MainActivity.java
+++ DBconnection/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dbconnection;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         Name=findViewById(R.id.et1);
         Surname=findViewById(R.id.et2);
         Mark=findViewById(R.id.et3);
+        DataV=findViewById(R.id.dv);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -35,9 +37,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+
     public void SaveData(View view) {
         String userVal=Name.getText().toString();
-        String surnameVal=Name.getText().toString();
+        String surnameVal=Surname.getText().toString();
         Integer passVal=Integer.parseInt(Mark.getText().toString());
         Boolean result=database.insertdata(userVal,passVal,surnameVal);
         if(result==true)
@@ -49,4 +53,43 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    public void read(View view){
+        Cursor res=database.getAllData();
+        StringBuffer sb=new StringBuffer();
+        if(res!=null&&res.getCount()>0){
+            while(res.moveToNext()){
+                sb.append("Id:"+res.getString(0)+"\n");
+                sb.append("Name:"+res.getString(1)+"\n");
+                sb.append("Surname:"+res.getString(2)+"\n");
+                sb.append("marks:"+res.getString(3)+"\n");
+
+            }
+            DataV.setText(sb.toString());
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Data Retrieved Successfully", Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(getApplicationContext(), "Data Retrieved Successfully", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void delete(View view){
+        String userVal=Name.getText().toString();
+        int result=database.deletedata(userVal);
+        Toast.makeText(getApplicationContext(), "Rows affected", Toast.LENGTH_SHORT).show();
+    }
+
+    public void update(View view){
+        String userVal=Name.getText().toString();
+        String surnameVal=Surname.getText().toString();
+        Integer passVal=Integer.parseInt(Mark.getText().toString());
+        Boolean result=database.updateData(userVal,passVal,surnameVal);
+        if(result==true){
+            Toast.makeText(getApplicationContext(), "Data Updated Successfully", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "No Rows Affected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
