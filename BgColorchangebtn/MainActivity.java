73a69abd5f2cddd2976e1c118855f47f31a229cc
merinmagi05 <144668865+merinmagi05@users.noreply.github.com>
2024-11-19 18:33:14 +0530

    Create MainActivity.java

---
+++ BgColorchangebtn/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.bgcolor;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ConstraintLayout main;
+    int r=0,g=0,b=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        main=findViewById(R.id.main);
+
+    }
+
+    public void changecolor(View view) {
+        r=(r+10)%256;
+        g=(g+20)%256;
+        b=(b+30)%256;
+        int color= Color.rgb(r,g,b);
+        main.setBackgroundColor(color);
+
+    }
+}
